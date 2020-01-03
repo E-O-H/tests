478472c25e055918864f22b7eb27597482713339
Chenyang Tang <ct1856@nyu.edu>
2020-01-02 22:21:59 -0500

    edit comment for static field

--- static field.cpp
+++ static field.cpp
@@ -8,6 +8,8 @@ static int b;
 
 int A::b = 3; // 必须在类体外初始化，且必须初始化，不能不经初始化就直接在main函数中赋值
               // 注意初始化语法带有类型，相当于定义全局变量，只不过绑定在类中
+// There is an exception: static const primative-type members can be initialized within the class.
+// In C++17 you can also use inline variables to initialize static member within the class, but it introduces a guard variable overhead.
 
 int main() {
 cout << A::b;
