0817fa5aaf09e97512b6b878f053c3d73ad4f692
Chenyang Tang <ct1856@nyu.edu>
2020-01-02 21:39:10 -0500

    ...

---
+++ virtual_helper_method.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+struct Base {
+    void fun() {
+        std::cout << "non-virtual helper: " 
+                  << nonvirtual_helper() 
+                  << std::endl;
+        std::cout << "virtual helper: " 
+                  << virtual_helper() 
+                  << std::endl;
+    }
+    int nonvirtual_helper() {
+        return 0;
+    }
+    virtual int virtual_helper() {
+        return 0;
+    }
+};
+
+struct Derived : public Base {
+    int nonvirtual_helper() {
+        return 1;
+    }
+    int virtual_helper() {
+        return 1;
+    }
+};
+
+int main() {
+    Derived d;
+    d.fun(); // 0 1
+    Derived* d_p = new Derived;
+    d_p->fun(); // 0 1
+    Base* b_p = new Derived;
+    b_p->fun(); // 0 1
+    // Note the results are the same.
+    // For non-virtual helper, the method in Base will always be invoked.
+}
