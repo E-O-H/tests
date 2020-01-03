2591eec02b72771038991107afaeffb7916c01b4
Chenyang Tang <ct1856@nyu.edu>
2020-01-02 22:09:34 -0500

    ...

--- virtual_helper_method.cpp
+++ virtual_helper_method.cpp
@@ -33,6 +33,13 @@ int main() {
     d_p->fun(); // 0 1
     Base* b_p = new Derived;
     b_p->fun(); // 0 1
-    // Note the results are the same.
-    // For non-virtual helper, the method in Base will always be invoked.
+    // Note the results are the same. For non-virtual helper, the method in Base will always be invoked.
+    // This is because the Derived class does not have its own fun() method,
+    // so when invoking fun() on a Derived object, the compiler implicitly static_cast the object/pointer
+    // to the type of their Base class (test in https://cppinsights.io/ to see it clearly),
+    // so that the method in the Base class could be invoked. Thus the this pointer passed to fun()
+    // is also a pointer to the Base class, and dynamic dispatch works on it for virtual helper functions called with it.
+    //////////////////
+    // If you instead copy fun() to the Derived class, result will be 1 1; 1 1; 0 1;
+    // If you copy fun() to Derived class, but make it a virtual method, result will be 1 1; 1 1; 1 1;
 }
