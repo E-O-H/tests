23d5294139b25ffac8d93db30119485f6e176cd0
Chenyang Tang <ct1856@nyu.edu>
2020-01-04 01:30:42 -0500

    exception in destructor

---
+++ exception in destructor.cpp
@@ -0,0 +1,28 @@
+﻿// Type your code here, or load an example.
+#include <iostream>
+
+void fun() {
+    throw int();
+}
+
+class A {
+    public:
+    ~A() {
+        try {
+            fun();
+        } catch(int) {
+            // If the int exception is not caught inside the destructor like this,
+            // the program will terminate once the float exception is thrown.
+            std::cout << "caught int" << std::endl;
+        }
+    }
+};
+
+int main() {
+    try {
+        A a;
+        throw float();
+    } catch (float) {
+        std::cout << "caught float" << std::endl;
+    }
+}
