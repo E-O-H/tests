fbc2967e78719e05b15337336d8d0befeb7d96e9
Chenyang Tang <ct1856@nyu.edu>
2020-01-05 06:36:13 -0500

    friend with inheritance of friend

---
+++ friend with inheritance of friend.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+class C;
+class Base {
+    virtual int a() {
+        return 3;
+    }
+    friend C;
+};
+class Derived : public Base {
+              // NOTE if there is no public, 
+              // compiler will give error "inaccessable base"!
+              // (The fact that we used it through a friend of base does not change the error!)
+    int a() {
+        return 4;
+    }
+};
+class C {
+    public: virtual void p(Base b) {
+        std::cout << b.a();
+    }
+};
+class D : public C {
+    ////// Following code does not compile, even if it is a virtual function!
+    // public: void p(Base b) {
+    //    std::cout << b.a();
+    // }
+};
