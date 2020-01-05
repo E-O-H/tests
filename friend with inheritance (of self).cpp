53c6e46c8247d274a9af1a48dcf96c0af761854c
Chenyang Tang <ct1856@nyu.edu>
2020-01-05 05:58:20 -0500

    friend with inheritance (of self)

---
+++ friend with inheritance (of self).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+class Base {
+    virtual int a() {
+        return 3;
+    }
+    friend void fun(Base*);
+};
+class Derived : public Base {
+              // NOTE if there is no public, 
+              // compiler will give error "inaccessable base"!
+              // (The fact that we used it through a friend of base does not change the error!)
+    int a() {
+        return 4;
+    }
+};
+void fun(Base* x) {
+    std::cout << x->a();
+}
+int main() {
+    Derived o;
+    fun(&o); // 4
+}
